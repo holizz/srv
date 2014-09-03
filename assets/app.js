736ed071fb6bfe00b8360f0b08efe1f45f58b3e1
Tom Adams <tom@holizz.com>
2014-09-03 19:33:04 -0400

    Add links

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,9 @@ var directory = angular.module('directory', [])
 
 directory.controller('DirectoryListing', function ($scope, $http) {
     $scope.path = document.location.pathname
+    if (!$scope.path.match(/\/$/)) {
+        $scope.path += '/'
+    }
 
     $scope.files = []
 
