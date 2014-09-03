9e4e919c7410b94af00b9ea922c5a90e4de0d6f2
Tom Adams <tom@holizz.com>
2014-09-03 18:29:27 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := flag.Int("p", 8000, "port to listen on")
+	dir := flag.String("d", ".", "directory to serve")
+	flag.Parse()
+
+	http.Handle("/", NewSrvServer(http.Dir(*dir)))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+}
+
+type SrvServer struct {
+	dir        http.Dir
+	fileServer http.Handler
+	html       string
+	js         string
+}
+
+func NewSrvServer(dir http.Dir) SrvServer {
+	html, err := ioutil.ReadFile("assets/index.html")
+	if err != nil {
+		panic(err)
+	}
+
+	js, err := ioutil.ReadFile("assets/app.js")
+	if err != nil {
+		panic(err)
+	}
+
+	s := SrvServer{
+		dir:        dir,
+		fileServer: http.FileServer(dir),
+		html:       string(html),
+		js:         string(js),
+	}
+
+	return s
+}
+
+func (s SrvServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/app.js" {
+		w.Header()["Content-Type"] = []string{"application/javascript; charset=utf-8"}
+		io.WriteString(w, s.js)
+		return
+	}
+
+	file, err := s.dir.Open(r.URL.Path)
+	if err != nil {
+		s.fileServer.ServeHTTP(w, r)
+		return
+	}
+	defer file.Close()
+
+	info, err := file.Stat()
+	if err != nil {
+		s.fileServer.ServeHTTP(w, r)
+		return
+	}
+
+	if info.IsDir() {
+		w.Header()["Content-Type"] = []string{"text/html; charset=utf-8"}
+		io.WriteString(w, s.html)
+		return
+	} else {
+		s.fileServer.ServeHTTP(w, r)
+		return
+	}
+}
