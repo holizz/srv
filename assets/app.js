71dd0352b60f3523bb9930d81f382f561f9697c6
Tom Adams <tom@holizz.com>
2014-09-03 19:28:01 -0400

    Very basically functions

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,15 @@
-hi
+var directory = angular.module('directory', [])
+
+directory.controller('DirectoryListing', function ($scope, $http) {
+    $scope.path = document.location.pathname
+
+    $scope.files = []
+
+    $http.get('/_srv/api?path='+encodeURI($scope.path))
+        .success(function (data) {
+            $scope.files = data
+        })
+        .error(function () {
+            alert("oh no")
+        })
+})
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -16,6 +17,10 @@ func main() {
 
 	http.Handle("/", NewSrvServer(http.Dir(*dir)))
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+}
+
+type File struct {
+	Path string `json:"path"`
 }
 
 type SrvServer struct {
@@ -47,7 +52,48 @@ func NewSrvServer(dir http.Dir) SrvServer {
 }
 
 func (s SrvServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/app.js" {
+	if r.URL.Path == "/_srv/api" {
+		paths := r.URL.Query()["path"]
+		if len(paths) > 0 {
+			file, err := s.dir.Open(paths[0])
+			if err != nil {
+				panic(err)
+			}
+			defer file.Close()
+
+			info, err := file.Stat()
+			if err != nil {
+				panic(err)
+			}
+
+			if !info.IsDir() {
+				panic(fmt.Errorf("oh no"))
+			}
+
+			files, err := file.Readdir(999) //TODO: 999 is too small
+			if err != nil {
+				panic(err)
+			}
+
+			outputFiles := []File{}
+			for _, f := range files {
+				outputFiles = append(outputFiles, File{
+					Path: f.Name(),
+				})
+			}
+
+			bytes, err := json.Marshal(outputFiles)
+			if err != nil {
+				panic(err)
+			}
+
+			w.Header()["Content-Type"] = []string{"application/json; charset=utf-8"}
+			io.WriteString(w, string(bytes))
+			return
+		}
+	}
+
+	if r.URL.Path == "/_srv/app.js" {
 		w.Header()["Content-Type"] = []string{"application/javascript; charset=utf-8"}
 		io.WriteString(w, s.js)
 		return
